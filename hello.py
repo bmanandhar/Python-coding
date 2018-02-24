c67c77bd29e704a0ea88378427a4f021002a5181
Bijaya <bijaya.manandhar@hotmail.com>
2018-02-23 18:48:39 -0800

    string involved or not

--- hello.py
+++ hello.py
@@ -6,3 +6,23 @@ This is a temporary script file.
 """
 x = 9; y = 10; z = 13
 print (x + 11 + y + 19 + z*3)
+
+None
+
+varA = "abcd"; varB = 1234
+
+if type(varA) == str or type(varB) == str:
+    print ("string involved")
+
+elif varA > varB:
+    print ("bigger")
+
+elif varA == varB:
+    print ("equal")
+
+else:
+    # If none of the above conditions are true,
+    # it must be the case that varA < varB
+    print ("smaller")
+''' End of code'''
+    
