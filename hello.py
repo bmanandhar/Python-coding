d00954706379100af34bcb5baa16bbf840d5d5fe
Bijaya <bijaya.manandhar@hotmail.com>
2018-02-23 18:59:44 -0800

    more ex

--- hello.py
+++ hello.py
@@ -40,4 +40,13 @@ while num <= 5:
     
 print("Outside of loop")
 print(num)
+#02
+
+numberOfLoops = 0
+numberOfApples = 2
+while numberOfLoops < 10:
+    numberOfApples *= 2
+    numberOfApples += numberOfLoops
+    numberOfLoops -= 1
+print("Number of apples: " + str(numberOfApples))
     
